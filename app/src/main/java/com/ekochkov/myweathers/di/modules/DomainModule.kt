62bdbebf502d7f8468cfa3f1c96a2a9a41a4c4ea
evgenykochkov <crabbs100@gmail.com>
2022-08-10 19:50:30 +0300

    add Dagger 2

---
+++ app/src/main/java/com/ekochkov/myweathers/di/modules/DomainModule.kt
@@ -0,0 +1,8 @@
+package com.ekochkov.myweathers.di.modules
+
+import dagger.Module
+
+@Module
+class DomainModule {
+    // тут будет предоставление интерактора для описания бизнес логики
+}
---
+++ app/src/main/java/com/ekochkov/myweathers/di/modules/RemoteModule.kt
@@ -0,0 +1,9 @@
+package com.ekochkov.myweathers.di.modules
+
+import dagger.Module
+import javax.inject.Singleton
+
+@Module
+class RemoteModule {
+    // тут будет предоставление объектов для работы с сетью
+}
