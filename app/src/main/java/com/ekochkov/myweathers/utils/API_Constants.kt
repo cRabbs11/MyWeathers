4b34f1598b5ec3b70ecbd057318d8a2c26aa5530
evgenykochkov <crabbs100@gmail.com>
2022-08-15 21:01:41 +0300

    created object for API constants

---
+++ app/src/main/java/com/ekochkov/myweathers/utils/API_Constants.kt
@@ -0,0 +1,7 @@
+package com.ekochkov.myweathers.utils
+
+object API_Constants {
+    const val OPEN_WEATHER_MAP_BASE_URL = "https://api.openweathermap.org"
+
+    const val OPEN_WEATHER_PARAMETER_API_KEY_NAME = "appid"
+}
