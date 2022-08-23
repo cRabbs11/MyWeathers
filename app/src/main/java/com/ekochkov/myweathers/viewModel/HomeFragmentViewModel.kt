d040f6bc490fb2768bd3924bde78d9e54cd3007a
evgenykochkov <crabbs100@gmail.com>
2022-08-23 15:40:11 +0300

    added methods for getting pointsListLiveData

--- app/src/main/java/com/ekochkov/myweathers/viewModel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/ekochkov/myweathers/viewModel/HomeFragmentViewModel.kt
@@ -3,21 +3,44 @@ package com.ekochkov.myweathers.viewModel
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ekochkov.myweathers.data.entity.Point
 import com.ekochkov.myweathers.domain.Interactor
+import com.ekochkov.myweathers.domain.ResponseCallback
 import com.ekochkov.myweathers.utils.App
 import javax.inject.Inject
 
 class HomeFragmentViewModel: ViewModel() {
 
-    val weatherLiveData = MutableLiveData<String>()
+    val pointsListLiveData = MutableLiveData<List<Point>>()
 
     @Inject
     lateinit var interactor: Interactor
 
     init {
-        Log.d("BMTH","init")
         App.instance.dagger.inject(this)
-
-        interactor.getWeatherByCoords("37.39", "-122.08")
     }
+
+    fun getCitiesNearbyLocation(lat: Double, lon: Double) {
+        interactor.getCities(object: ResponseCallback<List<Point>> {
+            override fun onSuccess(pointList: List<Point>) {
+                Log.d("BMTH","size = ${pointList.size}")
+                getWeather(pointList)
+            }
+
+            override fun onFailure() {
+            }
+        })
+    }
+
+    private fun getWeather(list: List<Point>) {
+        interactor.getWeatherByPoints(list, object: ResponseCallback<List<Point>> {
+            override fun onSuccess(item: List<Point>) {
+                pointsListLiveData.postValue(item)
+            }
+
+            override fun onFailure() {
+            }
+        })
+    }
+
 }
