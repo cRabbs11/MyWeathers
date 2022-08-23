ad41b6fc8e8a0130fa3dc5448ea28764fabc776e
evgenykochkov <crabbs100@gmail.com>
2022-08-23 15:36:25 +0300

    added geoDBRetrofit in Interactor constructor

--- app/src/main/java/com/ekochkov/myweathers/di/modules/DomainModule.kt
+++ app/src/main/java/com/ekochkov/myweathers/di/modules/DomainModule.kt
@@ -1,13 +1,17 @@
 package com.ekochkov.myweathers.di.modules
 
 import com.ekochkov.myweathers.domain.Interactor
+import com.ekochkov.myweathers.utils.GeoDBRetrofitInterface
 import com.ekochkov.myweathers.utils.OpenWeatherRetrofitInterface
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class DomainModule {
 
+    @Singleton
     @Provides
-    fun provideInteractor(weatherRetrofit: OpenWeatherRetrofitInterface) = Interactor(weatherRetrofit)
+    fun provideInteractor(weatherRetrofit: OpenWeatherRetrofitInterface,
+    geoDBRetrofit: GeoDBRetrofitInterface) = Interactor(weatherRetrofit, geoDBRetrofit)
 }
