efbdcf129359b690c05fef25d9931fa64855a7ee
evgenykochkov <crabbs100@gmail.com>
2022-09-04 18:45:27 +0300

    added Parcelable to Point.kt, Weather.kt

--- app/src/main/java/com/ekochkov/myweathers/data/entity/Weather.kt
+++ app/src/main/java/com/ekochkov/myweathers/data/entity/Weather.kt
@@ -1,10 +1,14 @@
 package com.ekochkov.myweathers.data.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Weather(
     val temp: Int,
     val temp_max: Int,
     val temp_min: Int,
     val description: String,
     val iconId: String
-) {
+): Parcelable {
 }
