c1a4186fa47c6ff6674b20206e5ec5041b292235
evgenykochkov <crabbs100@gmail.com>
2022-09-10 13:39:44 +0300

    added forecast request methods

--- app/src/main/java/com/ekochkov/myweathers/utils/OpenWeatherRetrofitInterface.kt
+++ app/src/main/java/com/ekochkov/myweathers/utils/OpenWeatherRetrofitInterface.kt
@@ -1,6 +1,7 @@
 package com.ekochkov.myweathers.utils
 
 import com.ekochkov.myweathers.data.entity.OpenWeatherCurrentDataDTO
+import com.ekochkov.myweathers.data.entity.OpenWeatherForecastDataDTO
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -15,6 +16,14 @@ interface OpenWeatherRetrofitInterface {
         @Query("units") units: String? = null,
         @Query("lang") lang: String? = null
     ) : Call<OpenWeatherCurrentDataDTO>
+
+    @GET("/data/2.5/forecast")
+    fun getForecastByCoord(
+        @Query("appid") appId: String,
+        @Query("lat") coordLat: String,
+        @Query("lon") coordLon: String,
+        @Query("units") units: String? = null,
+    ): Call<OpenWeatherForecastDataDTO>
 
 
 }
